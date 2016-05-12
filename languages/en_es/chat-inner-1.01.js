5220e06e41b75d3b4301c587fc2c15d846100bda
Peter Abrahamson <peter@atomjump.com>
2016-05-12 14:48:35 +1200

    provide for additional language switchover

---
+++ languages/en_es/chat-inner-1.01.js
@@ -0,0 +1,29 @@
+//Language & messages configuration
+//Note: also see /config/messages.json for further messages configuration
+var lsmsg = {
+    "defaultLanguage" : "en",
+    "msgs": {
+        "en":{
+              defaultYourName: 'Your Name',
+              defaultYourEmail: 'Your Email',
+              loggedIn: 'Logged in. Please wait..',
+              passwordWrong: 'Sorry, your password is not correct.',
+              passwordStored: 'Thanks, your password is now set.',
+              registration: 'Thanks for registering.  To confirm your email address we\'ve sent an email with a link in it, which you should click within a day.',
+              badResponse: 'Sorry, response is: ',
+              more: 'More'
+        },
+        "es":{
+              defaultYourName: 'Tu Nombre',
+              defaultYourEmail: 'Su e-mail',
+              loggedIn: 'Conectado Por favor, espere..',
+              passwordWrong: 'Lo siento, la contraseña no es correcta.',
+              passwordStored: 'Gracias, su contraseña se establece ahora.',
+              registration: 'Gracias por registrarse. Para confirmar su dirección de correo electrónico que\'ve enviado un correo electrónico con un enlace en ella, lo que debe hacer clic en un día.',
+              badResponse: 'Lo siento, la respuesta es: ',
+              more: 'Mas'
+        }       
+    }
+}
+
+//Note: this should be manually copied over what exists in js/chat-inner-1.01.js
---
+++ languages/en_es/chat.js
@@ -0,0 +1,23 @@
+//Language & messages configuration
+//Note: also see /config/messages.json for further messages configuration
+var lsmsg = {
+    "defaultLanguage" : "en",
+    "msgs": {
+        "en":{
+              privateMessage: 'Private Message',
+              settings: 'Settings',
+              uploadImagesTitle: 'Upload images or video.',
+              uploadImagesTitleSorry: 'Sorry image uploads are only supported on IE10+, or any other browser.',
+              sendTo: 'Send to'
+        },
+        "es":{
+              privateMessage: 'Mensaje Privado',
+              settings: 'Settings',
+              uploadImagesTitle: 'Sube imágenes o vídeo.',
+              uploadImagesTitleSorry: 'La subida de imágenes Lo siento, sólo se admiten en IE10+, o cualquier otro navegador.',
+              sendTo: 'Enviar a'
+        }      
+    }
+}
+
+//Note: this should be manually copied over what exists in Loop front-end's js/chat.js
