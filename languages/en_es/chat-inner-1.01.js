cc97415eab3487d6cb181ae4f9c8eb292542a11d
AtomJump <peter@atomjump.com>
2019-11-21 10:31:54 +1300

    Update chat-inner-1.01.js

--- languages/en_es/chat-inner-1.01.js
+++ languages/en_es/chat-inner-1.01.js
@@ -3,25 +3,37 @@
 var lsmsg = {
     "defaultLanguage" : "en",
     "msgs": {
-        "en":{
-              defaultYourName: 'Your Name',
-              defaultYourEmail: 'Your Email',
-              loggedIn: 'Logged in. Please wait..',
-              passwordWrong: 'Sorry, your password is not correct.',
-              passwordStored: 'Thanks, your password is now set.',
-              registration: 'Thanks for registering.  To confirm your email address we\'ve sent an email with a link in it, which you should click within a day.',
-              badResponse: 'Sorry, response is: ',
-              more: 'More'
+         "en":{
+              "defaultYourName": "Your Name",
+              "defaultYourEmail" : "Your Email",
+              "loggedIn": "Logged in. Please wait..",
+              "passwordWrong": "Sorry, your password is not correct.",
+              "forumPasswordWrong": "Sorry, your forum password is not correct.",
+              "passwordStored": "Thanks, your password is now set.",
+              "registration": "Thanks for registering.  To confirm your email address we've sent an email with a link in it, which you should click within a day.",
+              "badResponse": "Sorry, response is: ",
+              "more": "More",
+              "lostConnection": "Warning: Waiting for a good connection.",
+              "blankMessage": "Warning: you tried to send a blank message.",
+              "messageQueued": "Warning: your message 'MESSAGE' will be sent when a connection is re-established.",
+              "subscribed": "You have successfully subscribed.",
+              "subscriptionDenied": "Sorry, you are not authorised to subscribe to this forum."           
         },
         "es":{
-              defaultYourName: 'Tu Nombre',
-              defaultYourEmail: 'Su e-mail',
-              loggedIn: 'Conectado Por favor, espere..',
-              passwordWrong: 'Lo siento, la contraseña no es correcta.',
-              passwordStored: 'Gracias, su contraseña se establece ahora.',
-              registration: 'Gracias por registrarse. Para confirmar su dirección de correo electrónico que\'ve enviado un correo electrónico con un enlace en ella, lo que debe hacer clic en un día.',
-              badResponse: 'Lo siento, la respuesta es: ',
-              more: 'Mas'
+              "defaultYourName": "Tu Nombre",
+              "defaultYourEmail": "Su e-mail",
+              "loggedIn": "Conectado Por favor, espere..",
+              "passwordWrong": "Lo siento, la contraseña no es correcta.",
+              "forumPasswordWrong": "Lo sentimos, la contraseña de tu foro no es correcta.",
+              "passwordStored": "Gracias, su contraseña se establece ahora.",
+              "registration": "Gracias por registrarse. Para confirmar su dirección de correo electrónico que've enviado un correo electrónico con un enlace en ella, lo que debe hacer clic en un día.",
+              "badResponse": "Lo siento, la respuesta es: ",
+              "more": "Mas",
+              "lostConnection": "Advertencia: Esperando una buena conexión.",
+              "blankMessage": "Advertencia: ha intentado enviar un mensaje en blanco.",
+              "messageQueued": "Advertencia: su mensaje 'MESSAGE' será enviado cuando se restablezca una conexión.",
+              "subscribed": "Te has suscripto satisfactoriamente.",
+              "subscriptionDenied": "Lo sentimos, no estás autorizado para suscribirte a este foro."
         }       
     }
 }
