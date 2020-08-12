30242fd91b9f408fa66985afdd5cc07d56f7d7c8
AtomJump <peter@atomjump.com>
2020-08-13 09:57:14 +1200

    First stab at Chinese simplified

---
+++ languages/en_ch/chat-inner.js
@@ -0,0 +1,41 @@
+//Language & messages configuration
+//Note: also see /config/messages.json for further messages configuration
+var lsmsg = {
+    "defaultLanguage" : "en",
+    "msgs": {
+        "en":{
+              "defaultYourName": "Your Name",
+              "defaultYourEmail" : "Your Email",
+              "loggedIn": "Logged in. Please wait..",
+              "passwordWrong": "Sorry, your password is not correct.",
+              "forumPasswordWrong": "Sorry, your forum password is not correct.",
+              "passwordStored": "Thanks, your password is now set.",
+              "registration": "Thanks for registering.  To confirm your email address we've sent an email with a link in it, which you should click within a day.",
+              "badResponse": "Sorry, response is: ",
+              "more": "More",
+              "lostConnection": "Warning: Waiting for a good connection.",
+              "blankMessage": "Warning: you tried to send a blank message.",
+              "messageQueued": "Warning: your message 'MESSAGE' will be sent when a connection is re-established.",
+              "subscribed": "You have successfully subscribed.",
+              "subscriptionDenied": "Sorry, you are not authorised to subscribe to this forum."           
+        },
+        "ch": {
+        	 "defaultYourName": "您的名字",
+              "defaultYourEmail": "您的电子邮件",
+              "loggedIn": "已登录。请稍候..",
+              "passwordWrong": "对不起，您的密码不正确。",
+              "forumPasswordWrong": "对不起，您的论坛密码不正确。",
+              "passwordStored": "谢谢，现在已设置密码。",
+              "registration": "感谢您的注册。为确认您的电子邮件地址，我们已发送了一封带有链接的电子邮件，您应该在一天之内单击该链接。",
+              "badResponse": "对不起，响应为：",
+              "more": "更多",
+              "lostConnection": "警告：等待良好的连接。",
+              "blankMessage": "警告：您试图发送空白消息。",
+              "messageQueued": "警告：重新建立连接后，您的消息'MESSAGE'将被发送。",
+              "subscribed": "您已成功订阅。",
+              "subscriptionDenied": "很抱歉，您无权订阅此论坛。"
+        }     
+    }
+}
+
+//Note: this should be manually copied over what exists in js/chat-inner-x.y.js
---
+++ languages/en_ch/chats.js
@@ -0,0 +1,29 @@
+//Language & messages configuration
+//Note: also see /config/messages.json for further messages configuration
+var lsmsg = {
+    "defaultLanguage" : "en",
+    "msgs": {
+        "en":{
+              "privateMessage": "Private",
+              "settings": "Settings",
+              "uploadImagesTitle": "Upload images",
+              "uploadImagesTitleSorry": "Sorry image uploads are only supported on IE10+, or any other browser.",
+              "sendTo": "Send to",
+              "emoticonsTitle": "Emoticons",
+              "helpURL": "https://atomjump.com/wp/user-guide/",
+              "helpTitle": "Help"
+        },
+        "ch": {
+        	"privateMessage": "私人",
+            "settings": "设定值",
+            "uploadImagesTitle": "上传图像或视频。",
+            "uploadImagesTitleSorry": "仅在IE10 +或任何其他浏览器上支持对不起的图像上载。",
+            "sendTo": "发送到",
+            "emoticonsTitle": "表情图标",
+             "helpURL": "https://atomjump.com/wp/user-guide/",   
+             "helpTitle": "救命"
+        }     
+    }
+}
+
+//Note: this should be manually copied over what exists in Loop front-end's js/chat.js
